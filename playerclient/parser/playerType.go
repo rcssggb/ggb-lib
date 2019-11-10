3aef0e5cb57ff0d1300ddf8d393c444745f80975
Samuel Venzi <samuel.venzi@me.com>
2019-11-10 11:28:44 -0300

    Finish player type lexer and start parser

---
+++ playerclient/parser/playerType.go
@@ -0,0 +1,26 @@
+package parser
+
+// PlayerTypeData is the individual player information
+type PlayerTypeData struct {
+	ID             int
+	PlayerSpeedMax float64
+	StaminaIncMax  float64
+	PlayerDecay    float64
+	InertiaMoment  float64
+	DashPowerRate  float64
+	PlayerSize     float64
+	KickableMargin float64
+	KickRand       float64
+	ExtraStamina   float64
+	EffortMax      float64
+	EffortMin      float64
+	KickPowerRate  float64
+	FoulDetectProb float64
+	CatchableArea  float64
+}
+
+// PlayerType parses player type data coming from lexer
+func PlayerType(symbols map[string]string) (*PlayerTypeData, error) {
+
+	return nil, nil
+}
