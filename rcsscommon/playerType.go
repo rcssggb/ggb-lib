65d42eb4ee11e57bd918a95896221f8282782c48
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2020-03-09 22:38:54 -0300

    Define PlayerType struct type

--- rcsscommon/playerType.go
+++ rcsscommon/playerType.go
@@ -1,8 +1,19 @@
 package rcsscommon
 
-// PlayerType ...
+// PlayerType is the type containing all player type parameters
 type PlayerType struct {
-	// TODO: define struct to contain player type (to be put in a map[int]PlayerType for direct access by id)
+	PlayerSpeedMax        float64
+	StaminaIncMax         float64
+	PlayerDecay           float64
+	InertiaMoment         float64
+	DashPowerRate         float64
+	PlayerSize            float64
+	KickableMargin        float64
+	KickRand              float64
+	ExtraStamina          float64
+	EffortMax             float64
+	EffortMin             float64
+	KickPowerRate         float64
+	FoulDetectProbability float64
+	CatchableAreaLStretch float64
 }
-
-// (player_type (id 0)(player_speed_max 1.05)(stamina_inc_max 45)(player_decay 0.4)(inertia_moment 5)(dash_power_rate 0.006)(player_size 0.3)(kickable_margin 0.7)(kick_rand 0.1)(extra_stamina 50)(effort_max 1)(effort_min 0.6)(kick_power_rate 0.027)(foul_detect_probability 0.5)(catchable_area_l_stretch 1))
