bc797c67ba2727aeb3f2d9bb73ae07a2ae52eae8
Samuel Venzi <samuel.venzi@me.com>
2020-03-11 21:58:12 -0300

    Change errCh len

--- rcsscommon/playerType_test.go
+++ rcsscommon/playerType_test.go
@@ -5,8 +5,12 @@ import (
 )
 
 func TestPlayerType(t *testing.T) {
-	errCh := make(chan string)
+	errCh := make(chan string, 32)
 	playerTypeData := ParsePlayerType("(player_type (id 13)(player_speed_max 1.05)(stamina_inc_max 50.7145)(player_decay 0.403377)(inertia_moment 5.08442)(dash_power_rate 0.00504759)(player_size 0.3)(kickable_margin 0.635063)(kick_rand 0.0350625)(extra_stamina 59.3244)(effort_max 0.962702)(effort_min 0.562702)(kick_power_rate 0.027)(foul_detect_probability 0.5)(catchable_area_l_stretch 1.03477))\x00", errCh)
+
+	if len(errCh) != 0 {
+		t.Fail()
+	}
 
 	if playerTypeData == (PlayerType{}) {
 		t.FailNow()
